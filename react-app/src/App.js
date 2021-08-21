e92acc0dfe72ffb8834fe56259f48d32cf005287
danial <lipsky@knu.ua>
2021-08-21 15:34:05 +0300

    complite logic alert - show and hide

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import { Alert } from './components/Alert';
+import { AlertState } from './context/alert/AlertState';
 import { Navbar } from './components/Navbar';
 import { About } from './pages/About';
 import { Home } from './pages/Home';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Navbar />
-      <div className="container pt-4">
-        <Alert />
-        <Switch>
-          <Route path={'/'} exact component={Home}/>
-          <Route path={'/about'} exact component={About}/>
-        </Switch>
-      </div>
-   </BrowserRouter>
-    
+    <AlertState>
+      <BrowserRouter>
+        <Navbar />
+        <div className="container pt-4">
+          <Alert />
+          <Switch>
+            <Route path={'/'} exact component={Home}/>
+            <Route path={'/about'} exact component={About}/>
+          </Switch>
+        </div>
+      </BrowserRouter>
+   </AlertState>
   );
 }
 
--- react-app/src/components/Alert.js
+++ react-app/src/components/Alert.js
@@ -1,19 +1,21 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { AlertContext } from '../context/alert/alertContext'
 
-export const Alert = ({alert}) => {
+export const Alert = () => {
+    const {alert, hide} = useContext(AlertContext)
 
-if(!alert) {
-    return null
-}
+    if(!alert.visible) {
+        return null
+    }
 
     return(
-        <div className={`alert alert-${alert.type || 'warning'} alert-dismissible`}>
+        <div className={`alert alert-${alert.type || 'warning'} alert-dismissible note`}>
             <strong>Warning!</strong> 
             {alert.text}
-            
             <button 
+            onClick={hide}
             type="button" 
-            className="close" 
+            className="btn btn-lg "
             aria-label="Close"
             >
                 <span aria-hidden="true">&times;</span>
